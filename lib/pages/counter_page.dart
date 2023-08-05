60e192077b8a72601d5cff19dba927a94f220f9f
Denis Stahovsky <deniell@i.ua>
2023-08-05 23:57:59 +0300

    - added Counter page.

---
+++ lib/pages/counter_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_counter_app/main.dart';
+
+// ConsumerWidget is like a StatelessWidget
+// but with a WidgetRef parameter added in the build method.
+class CounterPage extends ConsumerWidget {
+  const CounterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Using the WidgetRef to get the counter int from the counterProvider.
+    // The watch method makes the widget rebuild whenever the int changes value.
+    //   - something like setState() but automatic
+    final int counter = ref.watch(counterProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter'),
+      ),
+      body: Center(
+        child: Text(
+          counter.toString(),
+          style: Theme.of(context).textTheme.displayMedium,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          // Using the WidgetRef to read() the counterProvider just one time.
+          //   - unlike watch(), this will never rebuild the widget automatically
+          // We don't want to get the int but the actual StateNotifier, hence we access it.
+          // StateNotifier exposes the int which we can then mutate (in our case increment).
+          ref.read(counterProvider.notifier).state++;
+        },
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:riverpod_counter_app/pages/counter_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
