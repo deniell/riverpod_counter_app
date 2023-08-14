60d489879f911bd857e18bead95fc80e500d883b
Denis Stahovsky <deniell@i.ua>
2023-08-14 23:58:26 +0300

    - counter functionality is ready on StateNotifierCounterPage.

--- lib/pages/state_notifier_counter_page.dart
+++ lib/pages/state_notifier_counter_page.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_counter_app/providers/state_notifier_counter_provider.dart';
 
-class StateNotifierCounterPage extends StatelessWidget {
-  const StateNotifierCounterPage({Key key}) : super(key: key);
+class StateNotifierCounterPage extends ConsumerWidget {
+  const StateNotifierCounterPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('State Notifier Counter'),
+        // actions: [
+        //   IconButton(
+        //     onPressed: () {
+        //     },
+        //     icon: const Icon(Icons.refresh),
+        //   )
+        // ],
+      ),
+      body: Center(
+        child: Consumer(
+          builder: (context, ref, child) {
+            final count = ref.watch(stateNotifierCounterProvider);
+            final text = count == null ? "Press the button" : count;
+            return Text(
+              text.toString(),
+              style: Theme.of(context).textTheme.displayMedium,
+            );
+          }
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: ref.read(stateNotifierCounterProvider.notifier).increment,
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
--- lib/providers/state_notifier_counter_provider.dart
+++ lib/providers/state_notifier_counter_provider.dart
@@ -1,5 +1,22 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+extension OptionalInfixAdition<T extends num> on T? {
+  T? operator + (T? other) {
+    final shadow = this;
+    if (shadow != null) {
+      return shadow + (other ?? 0) as T;
+    } else {
+      return null;
+    }
+  }
+}
+
+class Counter extends StateNotifier<int?> {
+  Counter() : super(null);
+  void increment() => state = state == null ? 1 : state + 1;
+  int? get value => state;
+}
 
 final stateNotifierCounterProvider = StateNotifierProvider<Counter, int?>(
-        (ref) => Counter(),
+  (ref) => Counter(),
 );
